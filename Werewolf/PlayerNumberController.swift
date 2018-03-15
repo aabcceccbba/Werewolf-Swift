ee2f9248e216c56649e332661f44ffebb6958e2a
aabcceccbba <kaiyuelu@gmail.com>
2018-03-15 02:32:38 -0400

    add roleController view

---
+++ Werewolf/PlayerNumberController.swift
@@ -0,0 +1,49 @@
+//
+//  PlayerNumberController.swift
+//  Werewolf
+//
+//  Created by Kay Lu on 3/14/18.
+//  Copyright © 2018 a. All rights reserved.
+//
+
+import UIKit
+
+class PlayerNumberController: UIViewController {
+
+    /*@IBOutlet weak var playerNumberTF: UITextField!
+    
+    @IBAction func enterNumber(_ sender: Any) {
+        if playerNumberTF.text != ""
+        {
+     
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextController = segue.destination as! RoleConformationController
+        nextController.player = Int(playerNumberTF.text!)!
+    }*/
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Werewolf/PlayerNumberViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PlayerNumberViewController.swift
-//  Werewolf
-//
-//  Created by Kay Lu on 3/7/18.
-//  Copyright © 2018 a. All rights reserved.
-//
-
-import Foundation
