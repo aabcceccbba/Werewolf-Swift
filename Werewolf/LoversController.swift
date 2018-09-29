e2afdc2d5146539dfc26e461e8662b3ba9895149
aabcceccbba <kaiyuelu@gmail.com>
2018-09-29 10:58:27 -0400

    Modified Cupido and add LoversController

---
+++ Werewolf/LoversController.swift
@@ -0,0 +1,30 @@
+//
+//  LoversController.swift
+//  Werewolf
+//
+//  Created by Kay Lu on 9/29/18.
+//  Copyright © 2018 a. All rights reserved.
+//
+
+import UIKit
+
+class LoversController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
