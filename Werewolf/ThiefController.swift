a0854e56eecd4d16ee136c0fe29565c11d504f86
aabcceccbba <kaiyuelu@gmail.com>
2019-01-21 17:46:57 -0500

    Add Picker View on Thief page

--- Werewolf/ThiefController.swift
+++ Werewolf/ThiefController.swift
@@ -8,10 +8,33 @@
 
 import UIKit
 
-class ThiefController: UIViewController {
+class ThiefController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet weak var night_falls: UILabel!
     @IBOutlet weak var thiefTF: UITextField!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    let roles = ["Werewolf", "Townfolk", "Fortune Teller", "Hunter", "Cupdo", "Witch", "Little Girl"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        // number of rows in the picker view
+        return 1;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        // content of each row
+        return roles[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        // number of rows
+        return roles.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // display something when select
+        // optional
+    }
     
     var playerNumber = PlayerNumberController.num
     var thiefID = Int()
