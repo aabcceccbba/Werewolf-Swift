daf23525082631485cf584b694cfec85b0e98935
aabcceccbba <kaiyuelu@gmail.com>
2020-02-09 17:57:01 -0600

    add TransferController

---
+++ Werewolf/TransferController.swift
@@ -0,0 +1,30 @@
+//
+//  TransferController.swift
+//  Werewolf
+//
+//  Created by Kay Lu on 2/9/20.
+//  Copyright © 2020 a. All rights reserved.
+//
+
+import UIKit
+
+class TransferController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Werewolf/VoteController.swift
+++ Werewolf/VoteController.swift
@@ -10,15 +10,55 @@ import UIKit
 
 class VoteController: UIViewController {
 
+    @IBOutlet weak var eliminateTF: UITextField!
+    
     override func viewDidLoad() {
-        navigationController?.navigationBar.isUserInteractionEnabled = false
-        navigationController?.navigationBar.tintColor = UIColor.lightGray
+        // hide the back button to prevent going back
+        navigationItem.hidesBackButton = true
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        // empty input
+        if eliminateTF.text == "" {
+            let alert = UIAlertController(title: "Please enter the right retaliated Victim ID", message: "Retaliated Victim ID should be only between 1 and " + String(PlayerNumberController.num), preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+            
+            return false
+        }
+        
+        let eliminate = Int(eliminateTF.text!)!
+        // out of range
+        if eliminate > PlayerNumberController.num || eliminate < 1 {
+            let alert = UIAlertController(title: "Please enter the right retaliated Victim ID", message: "Retaliated Victim ID should be only between 1 and " + String(PlayerNumberController.num), preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+            return false
+        }
+        
+        // already dead
+        if !RolesController.alive.contains(eliminate) {
+            let alert = UIAlertController(title: "Please enter the right retaliated Victim ID", message: "This player is already dead.", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+            return false
+        }
+        
+        // update alive
+    RolesController.alive.remove(RolesController.potentialVictim)
+        // lover update...
+        
+        return true
+    }
     /*
     // MARK: - Navigation
 
